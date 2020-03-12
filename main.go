853c4bba7bcb470b135a5d5db68b7a5847ad3235
Bartosz Malcherek <bartoszmalcherek@outlook.com>
2020-03-12 14:26:48 +0100

    /images/ endpoint that returns 1 image from redisDB and respons with json

--- main.go
+++ main.go
@@ -2,15 +2,20 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
+	//"fmt"
 	//"io/ioutil"
 	"log"
 	"net/http"
 
 	"github.com/gomodule/redigo/redis"
+	"github.com/gorilla/mux"
 )
 
 var API_KEY string = "Mv61fomCvGOpcb01LyHq02MziK10cv6tCMxdyfox"
+var (
+	conn redis.Conn
+	err  error
+)
 
 type APOD struct {
 	Copyright       string
@@ -21,6 +26,14 @@ type APOD struct {
 	Service_version string
 	Title           string
 	Url             string
+}
+
+func init() {
+	log.Println("Hello from init")
+	conn, err = redis.Dial("tcp", "localhost:6379")
+	if err != nil {
+		log.Fatal(err)
+	}
 }
 
 func getUrl(url string) APOD {
@@ -45,18 +58,46 @@ func getUrl(url string) APOD {
 	return apod
 }
 
-func main() {
-	conn, err := redis.Dial("tcp", "localhost:6379")
+func images(w http.ResponseWriter, r *http.Request) {
+	log.Println("New connection")
+	var img APOD
+	image, err := redis.Values(conn.Do("HGETALL", "APOD:test"))
 	if err != nil {
 		log.Fatal(err)
 	}
+	if err = redis.ScanStruct(image, &img); err != nil {
+		log.Fatal(err)
+	}
+
+	res, err := json.Marshal(img)
+	if err != nil {
+		log.Fatal(err)
+	}
+	w.WriteHeader(http.StatusOK)
+	w.Write(res)
+}
+
+func home(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("Hello World!"))
+}
+
+func main() {
 	defer conn.Close()
 
-	image := getUrl("https://api.nasa.gov/planetary/apod")
-	_, err = conn.Do("HSET", redis.Args{"APOD:test"}.AddFlat(image)...)
+	r := mux.NewRouter()
+	r.HandleFunc("/", home).Methods("GET")
+	r.HandleFunc("/images/", images).Methods("GET")
+	err = http.ListenAndServe(":8080", r)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	fmt.Println()
+	/* image := getUrl("https://api.nasa.gov/planetary/apod")*/
+	//_, err = conn.Do("HSET", redis.Args{"APOD:test"}.AddFlat(image)...)
+	//if err != nil {
+	//log.Fatal(err)
+	//}
+
+	/*fmt.Println()*/
 }
